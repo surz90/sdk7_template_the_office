deed0f618413b46bd8420209cd9d68c13773996a
surz90 <sceloglaux.90@gmail.com>
2023-12-19 22:41:42 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// We define the empty imports so the auto-complete feature works as expected.
+import { } from '@dcl/sdk/math'
+import { Animator, AudioSource, AvatarAttach, GltfContainer, Material, Transform, VideoPlayer, VisibilityComponent, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
+import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
+
+import { changeColorSystem, circularSystem } from './systems'
+import { setupUi } from './ui'
+
+// You can remove this if you don't use any asset packs
+initAssetPacks(engine, pointerEventsSystem, {
+  Animator,
+  AudioSource,
+  AvatarAttach,
+  Transform,
+  VisibilityComponent,
+  GltfContainer,
+  Material,
+  VideoPlayer
+})
+
+export function main() {
+//   // Defining behavior. See `src/systems.ts` file.
+//   engine.addSystem(circularSystem)
+//   engine.addSystem(changeColorSystem)
+
+//   // draw UI. Here is the logic to spawn cubes.
+//   setupUi()
+}
